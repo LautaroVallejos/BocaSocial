cdb9b57f8d33a58de167e5725b68ac6652466bad
nicolas-carlini <nicolascarlini1@gmail.com>
2020-05-15 18:13:05 -0300

    update

--- controllers/auth.crtl.js
+++ controllers/auth.crtl.js
@@ -5,6 +5,7 @@ const router = Router();
 
 const jwt = require('jsonwebtoken');
 const config = require('../config');
+const geoip = require("geoip-lite");
 
 const User = require('../models/User');
 
